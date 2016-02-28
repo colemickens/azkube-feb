7591ef5d2474c886b5cc4505c1239c3afcecdac0
Cole Mickens <cole.mickens@gmail.com>
2016-02-28 03:36:51 -0800

    rework

--- Makefile
+++ Makefile
@@ -1,17 +1,5 @@
 .NOTPARALLEL:
 
-workTenantId := 72f988bf-86f1-41af-91ab-2d7cd011db47
-workSubscriptionId := 27b750cd-ed43-42fd-9044-8d75e124ae55
-workClientId := undefined
-workClientSecret := undefined
-
-# personal client id is 'azkube-ci'
-# this application runs as 'azkube'
-personalTenantId := 13de0a15-b5db-44b9-b682-b4ba82afbd29
-personalSubscriptionId := aff271ee-e9be-4441-b9bb-42f5af4cbaeb
-personalClientId := 20f97fda-60b5-4557-9100-947b9db06ec0
-personalClientSecret := $(shell cat clientsecret)
-
 all: build
 
 glide:
@@ -23,11 +11,6 @@ build:
 	CGO_ENABLED=0 \
 	go build -a -tags netgo -installsuffix nocgo -ldflags '-w' .
 
-quick:
-	GO15VENDOREXPERIMENT=1 \
-	CGO_ENABLED=0 \
-	go build .
-
 docker: build
 	docker build -t azkube .
 
@@ -35,36 +18,5 @@ docker-push: docker
 	docker tag -f azkube "colemickens/azkube:latest"
 	docker push "colemickens/azkube"
 
-deploy-cs-personal:
-	go run main.go deploy \
-		--tenant-id="$(personalTenantId)" \
-		--subscription-id="$(personalSubscriptionId)" \
-		--auth-method="clientsecret" \
-		--client-id="$(personalClientId)" \
-		--client-secret="$(personalClientSecret)"
-
-deploy-device-personal:
-	go run main.go deploy \
-		--tenant-id="$(personalTenantId)" \
-		--subscription-id="$(personalSubscriptionId)" \
-		--auth-method="device"
-
-deploy-cs-work:
-	go run main.go deploy \
-		--tenant-id="$(workTenantId)" \
-		--subscription-id="$(workSubscriptionId)" \
-		--auth-method="clientsecret" \
-		--client-id="$(workClientId)" \
-		--client-secret="$(workClientSecret)"
-
-deploy-device-work:
-	go run main.go deploy \
-		--tenant-id="$(workTenantId)" \
-		--subscription-id="$(workSubscriptionId)" \
-		--auth-method="device"
-
 clean:
-	rm -f azkube
-	rm -f state.json
-
-clean-run: clean run
+	rm azkube
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/colemickens/azkube/util"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 const (
@@ -26,6 +28,17 @@ func NewRootCmd() *cobra.Command {
 	rootCmd.PersistentFlags().StringVar(&rootArgs.ClientCertificatePath, "certificate-path", "", "path to client certificate (used with --auth-method=clientcertificate)")
 	rootCmd.PersistentFlags().StringVar(&rootArgs.PrivateKeyPath, "private-key-path", "", "path to private key (used with --auth-method=clientcertificate)")
 
+	viper.SetEnvPrefix("azkube_")
+	viper.BindEnv("tenant_id")
+	viper.BindEnv("subscription_id")
+	viper.BindEnv("client_secret")
+	viper.BindEnv("client_certificate_path")
+	viper.BindPFlag("tenant_id", rootCmd.PersistentFlags().Lookup("tenant-id"))
+	viper.BindPFlag("subscription_id", rootCmd.Flags().Lookup("subscription-id"))
+	viper.BindPFlag("client_id", rootCmd.PersistentFlags().Lookup("client-id"))
+	viper.BindPFlag("client_secret", rootCmd.PersistentFlags().Lookup("client-secret"))
+	viper.BindPFlag("client_certificate_path", rootCmd.PersistentFlags().Lookup("client-certificate-path"))
+
 	rootCmd.AddCommand(NewDeployCmd())
 	//rootCmd.AddCommand(NewScaleDeploymentCmd())
 	//rootCmd.AddCommand(NewDestroyDeploymentCmd())
@@ -34,6 +47,22 @@ func NewRootCmd() *cobra.Command {
 }
 
 func validateRootArgs(rootArgs util.RootArguments) {
-	// noop for now
-	// validate auth_type, etc
+	if rootArgs.SubscriptionID == "" {
+		log.Fatal("--subscription-id must be specified")
+	}
+
+	if rootArgs.TenantID == "" {
+		log.Fatal("--tenant-id must be specified")
+	}
+
+	if rootArgs.AuthMethod == "client-secret" {
+		if rootArgs.ClientID == "" || rootArgs.ClientSecret == "" {
+			log.Fatal("--client-id and --client-secret must be specified when --auth-method=\"client_secret\"")
+		}
+	} else if rootArgs.AuthMethod == "client-certificate" {
+		if rootArgs.ClientID == "" || rootArgs.ClientCertificatePath == "" {
+			log.Fatal("--client-id and --client-certificate must be specified when --auth-method=\"client_certificate\"")
+		}
+
+	}
 }
